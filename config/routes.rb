8c13a350f919a238ee50c77661b8bd066d27cae5
George Gloyens <georgegloyens@gmail.com>
2022-11-18 16:19:15 +0000

    buttons and css

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :lists, only: %i[index new show create] do
+  resources :lists, only: %i[index new show create destroy] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
